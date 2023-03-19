37abeb6a01308cb166e08c347f9fd775684385d9
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-03-19 22:11:36 +0800

    added table and imgs

--- docx_utils.py
+++ docx_utils.py
@@ -7,9 +7,11 @@ from utils_functions.create_image import create_image
 def create_word_template_with_image(output_path, img_paths):
     doc = docx.Document()
 
-    # Add table with name, age, and gender
-    table = create_table(doc, 1, 3)
-    add_table_row(table, ["姓名：", "年龄：", "性别："])
+    create_title(doc, "足踝检测评估报告", 1)
+    # Add the contents for the first row of the table
+    table = create_table(doc, 0, 3)
+    add_table_row(table, ["姓名：\t", "年龄：\t", "性别："])
+
     create_image(doc, img_paths[0], 6)
     doc.add_page_break()
 
@@ -26,8 +28,13 @@ def create_word_template_with_image(output_path, img_paths):
 
     create_title(doc, "足跟内外翻情况对比：", 2)
     compare_table2 = create_table(doc, 1, 2)
-    add_image_with_title(doc, img_paths[4], "正常后跟内外翻情况 ", compare_table2.cell(0, 0))
+    add_image_with_title(doc, img_paths[4], "正常后跟内外翻情况", compare_table2.cell(0, 0))
     add_image_with_title(doc, img_paths[5], "实际后跟内外翻情况", compare_table2.cell(0, 1))
+
+    # Add text after the tables
+    create_title(doc, "初步诊断：", 2)
+    doc.add_paragraph("足弓低平且足后跟外偏，提示可能有运动损伤风险。处于生长发育阶段的儿童及青少年，应使用有矫正效果的鞋垫，并搭配合适的鞋类，日常应加强足部及腿部肌肉锻炼、拉伸及放松，以改善足弓扁平及足部外翻的情况，防止足部问题因体重增长及不良步态导致进行性加重，从而影响膝、骨盆与脊柱的正常生物力线；\n\n足部问题常伴有体态姿势不良等问题的出现，处于生发育阶段的儿童及青少年应每3个月到半年进行一次全面的脊柱、体态、骨龄及足部的相关检查。")
+    create_image(doc, img_paths[6], 6)
 
     doc.save(output_path)
 
